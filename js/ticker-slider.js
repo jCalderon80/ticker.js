d190967415be43270d325050a08bba622e20f247
Johnny Calderon <jcalderon@iguana-web.net>
2014-04-28 19:57:24 -0500

    fixes

--- js/ticker-slider.js
+++ js/ticker-slider.js
@@ -13,7 +13,7 @@ function ticker( options ) {
 	var strip;
 	var in_move;
 	var actual_position;
-	var id = optiond.id;
+	var id = options.id;
 	var orig_pos = 0;
 	var	stop = ( options.stop_on_hover == "" || options.stop_on_hover == null ) ? true : options.stop_on_hover;
 	var	orig_speed = ( options.speed == "" || options.speed == null ) ? 100 : options.speed;
@@ -26,7 +26,7 @@ function ticker( options ) {
 		//Get all elements
 		var roller, l_arr, r_arr, arrows;
 		
-		roller = document.getElementById("ticker-roller");
+		roller = document.getElementById( "ticker-roller" );
 		
 		//Event Listeners for Left and Right Speeders
 		l_arr = document.getElementsByClassName("left-speeder");
@@ -81,92 +81,96 @@ function ticker( options ) {
 			in_move = setInterval( function() {
 				run_strip();
 			}, speed );
-		}, false )
-		
-	
-		function set_rollers( roller ) {
-			
-			var elems = roller.getElementsByTagName( "li" );
-			
-			for( var i = 0; i < elems.length; i++ ) {
-				roller_width += elems[i].offsetWidth;
-			}
-			roller_width += 1
-			
-			orig_pos = -(roller_width);
-			
-			strip.style.width = ( ( window.roller_width * 3 ) + 1 ) + "px";
-			strip.style.marginLeft = "-" + window.roller_width + "px";
-			
-			roller.style.width = window.roller_width + "px";
-			
-			triplicate_it( roller_width );
-			
-			function triplicate_it( width ) {
-				
-				var elems = roller.innerHTML;
-				var x = 0;
-				
-				strip = document.getElementById("ticker-strip");
-				
-				while ( x < 2) {
-					
-					var newRoller = document.createElement("ul");
-					newRoller.setAttribute("id", "ticker-roller");
-					newRoller.style.width = window.roller_width;
-					newRoller.innerHTML = elems;
-					newRoller.style.width = width;
-					strip.appendChild( newRoller );
-					
-					x++;
-					
-				}
-			}
-		
-		}
-		
-		function pause_strip() {
-			if ( stop === true ) {
-				window.clearInterval( in_move );
-			}
-		}
-		
-		function run_strip() {
-			
-			actual_position = ( strip.style.marginLeft == "" ) ? 0 : parseInt( strip.style.marginLeft);
-			
-			if ( dir == "ltr" ) {
-				
-				if ( actual_position == 0 ) {
-					actual_position = orig_pos;
-				} else {
-					actual_position = actual_position + 1;
-				}
-					
-			} else {
-				
-				if ( actual_position == ( orig_pos * 2 ) ) {
-					actual_position = orig_pos;
-				} else {
-					actual_position = actual_position - 1;
-				}
-					
-			}
-			
-			strip.style.marginLeft = actual_position + "px";
-			
-		}
+		}, false );
 		
 		set_rollers( roller )
-		
+	
 		window.setInterval( function() {
 			run_strip();
 		}, speed );
 		
 	} //window.onload
-} //strip_
+	
+	function set_rollers( roller ) {
+		//
+			
+		var elems = roller.getElementsByTagName( "li" );
+		
+		for( var i = 0; i < elems.length; i++ ) {
+			roller_width += elems[i].offsetWidth;
+			console.log(roller_width);
+		}
+		roller_width += 1
+		
+		console.log(roller_width);
+		
+		orig_pos = -(roller_width);
+		
+		strip.style.width = ( ( window.roller_width * 3 ) + 1 ) + "px";
+		strip.style.marginLeft = "-" + window.roller_width + "px";
+		
+		roller.style.width = window.roller_width + "px";
+		
+		triplicate_it( roller_width, roller );	
+	}
+	
+	function triplicate_it( width, roller ) {
+		//triplicate strip content
+		
+		var elems = roller.innerHTML;
+		var x = 0;
+		
+		strip = document.getElementById("ticker-strip");
+		
+		while ( x < 2) {
+			
+			var newRoller = document.createElement("ul");
+			newRoller.setAttribute("id", "ticker-roller");
+			newRoller.style.width = window.roller_width;
+			newRoller.innerHTML = elems;
+			newRoller.style.width = width;
+			strip.appendChild( newRoller );
+			
+			x++;
+			
+		}
+	}
+	
+	function pause_strip() {
+		if ( stop === true ) {
+			window.clearInterval( in_move );
+		}
+	}
+	
+	function run_strip() {
+		
+		actual_position = ( strip.style.marginLeft == "" ) ? 0 : parseInt( strip.style.marginLeft);
+		
+		if ( dir == "ltr" ) {
+			
+			if ( actual_position == 0 ) {
+				actual_position = orig_pos;
+			} else {
+				actual_position = actual_position + 1;
+			}
+				
+		} else {
+			
+			if ( actual_position == ( orig_pos * 2 ) ) {
+				actual_position = orig_pos;
+			} else {
+				actual_position = actual_position - 1;
+			}
+				
+		}
+		
+		strip.style.marginLeft = actual_position + "px";
+		
+	}	
+} //ticker
 
 ticker({
+	id: "ticker-a",
 	speed: 20,
 	stop_on_hover: true,
 	direction: "rtl",
